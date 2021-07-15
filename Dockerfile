71323f9d3d677551a4fad8235d0bf667c6c8ec09
Enol Fernández <enol.fernandez@egi.eu>
2021-07-15 10:59:20 +0100

    New D4Science auth (#53)
    * New D4Science auth
    
    * Fixes after debugging
    
    * Bring the spawner in
    
    * Fix imports and linting
    
    * Update egi_notebooks_hub/d4science.py
    
    Co-authored-by: Andrea Manzi <andrea.manzi@gmail.com>
    
    * Add env variables
    
    * Fix async stuff
    
    * Fixes
    
    * Black-me
    
    * Filter out profiles according to permissions
    
    * Remove unused imports
    
    * Update D4Science API url
    
    * Rename file with static assets
    
    * some review fixes
    
    * Do not overwrite jupyterhub templates
    
    Configuration needs now to specify where to find templates
    (/egi-notebooks-hub/templates) and will allow to easily override the
    config for those special cases (d4science)
    
    * set d4science context as pod annotation
    
    * Fix variable name
    
    * Upgrade base image
    
    * Request multiple tokens
    
    * Get right token for WS mounting
    
    * use better name for the sidecar
    
    * Avoid runAsGroup
    
    Co-authored-by: Andrea Manzi <andrea.manzi@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Starting with the image used in helm jupyterhub
-FROM jupyterhub/k8s-hub:0.11.1
+FROM jupyterhub/k8s-hub:1.0.0
 
 USER root
 
@@ -9,16 +9,8 @@ COPY . /egi-notebooks-hub/
 # hadolint ignore=DL3013
 RUN pip3 install --no-cache-dir /egi-notebooks-hub
 
-# Customise Jupyter login
-# TODO(enolfc): this can break quite easily, should find a better way
-RUN cp /egi-notebooks-hub/templates/login.html /usr/local/share/jupyterhub/templates/login.html \
-    && cp /egi-notebooks-hub/templates/401.html /usr/local/share/jupyterhub/templates/401.html \
-    && cp /egi-notebooks-hub/templates/401.html /usr/local/share/jupyterhub/templates/403.html \
-    && cp /egi-notebooks-hub/images/egi-notebooks.png /usr/local/share/jupyterhub/static/images/jupyter.png \
-    && cp /egi-notebooks-hub/images/notebooks-logo.png /usr/local/share/jupyterhub/static/images/notebooks-logo.png \
-    && cp /egi-notebooks-hub/images/cesnet.png /usr/local/share/jupyterhub/static/images/cesnet.png \
-    && cp /egi-notebooks-hub/images/cesga.jpg /usr/local/share/jupyterhub/static/images/cesga.jpg \
-    && cp /egi-notebooks-hub/images/infn-ct.jpg /usr/local/share/jupyterhub/static/images/infn-ct.jpg
+# Copy images to the right place so they are found
+RUN cp -r /egi-notebooks-hub/static/* /usr/local/share/jupyterhub/static/
 
 ARG NB_USER=jovyan
 USER ${NB_USER}
--- egi_notebooks_hub/d4science.py
+++ egi_notebooks_hub/d4science.py
@@ -1,37 +1,41 @@
 """D4Science Authenticator for JupyterHub
 """
 
+import base64
 import datetime
 import json
 import os
+from urllib.parse import unquote, urlencode
 from xml.etree import ElementTree
 
+import jwt
 from jupyterhub.auth import Authenticator
 from jupyterhub.handlers import BaseHandler
 from jupyterhub.utils import url_path_join
-from tornado import gen, web
+from kubespawner import KubeSpawner
+from oauthenticator.generic import GenericOAuthenticator
+from oauthenticator.oauth2 import OAuthLoginHandler
+from tornado import web
 from tornado.httpclient import AsyncHTTPClient, HTTPError, HTTPRequest
 from tornado.httputil import url_concat
+from traitlets import Unicode
 
 D4SCIENCE_SOCIAL_URL = os.environ.get(
     "D4SCIENCE_SOCIAL_URL",
-    "https://socialnetworking1.d4science.org/" "social-networking-library-ws/rest/",
+    "https://api.d4science.org/social-networking-library-ws/rest/",
 )
-
-
 D4SCIENCE_PROFILE = "2/people/profile"
-
-
 D4SCIENCE_DM_REGISTRY_URL = os.environ.get(
     "D4SCIENCE_REGISTRY_URL",
     "https://registry.d4science.org/icproxy/gcube/"
     "service/ServiceEndpoint/DataAnalysis/DataMiner",
 )
-
-
 D4SCIENCE_DISCOVER_WPS = os.environ.get(
     "D4SCIENCE_DISCOVER_WPS",
     "false",
+)
+D4SCIENCE_OIDC_URL = os.environ.get(
+    "D4SCIENCE_OIDC_URL", "https://accounts.d4science.org/auth/realms/d4science/"
 )
 
 
@@ -45,10 +49,9 @@ class D4ScienceLoginHandler(BaseHandler):
             name, value="", path=path, expires=expires, domain=domain, **kwargs
         )
 
-    @gen.coroutine
-    def get(self):
+    async def get(self):
         self.log.debug("Authenticating user")
-        user = yield self.get_current_user()
+        user = await self.get_current_user()
         token = self.get_argument("gcube-token")
         if user and token:
             self.log.debug("Clearing login cookie, new user?")
@@ -75,7 +78,7 @@ class D4ScienceLoginHandler(BaseHandler):
         )
         req = HTTPRequest(user_url, method="GET")
         try:
-            resp = yield http_client.fetch(req)
+            resp = await http_client.fetch(req)
         except HTTPError as e:
             # whatever, get out
             self.log.warning("Something happened with gcube service: %s", e)
@@ -98,7 +101,7 @@ class D4ScienceLoginHandler(BaseHandler):
             req = HTTPRequest(discovery_url, method="GET")
             try:
                 self.log.debug("fetch")
-                resp = yield http_client.fetch(req)
+                resp = await http_client.fetch(req)
             except HTTPError as e:
                 # whatever, get out
                 self.log.error("Something happened with gcube service: %s", e)
@@ -122,7 +125,7 @@ class D4ScienceLoginHandler(BaseHandler):
             "context": context,
         }
         data.update(resp_json["result"])
-        user = yield self.login_user(data)
+        user = await self.login_user(data)
         if user:
             self._jupyterhub_user = user
             self.redirect(self.get_next_url(user), permanent=False)
@@ -130,24 +133,222 @@ class D4ScienceLoginHandler(BaseHandler):
 
 class D4ScienceAuthenticator(Authenticator):
     login_handler = D4ScienceLoginHandler
-    auto_login = True
 
-    @gen.coroutine
-    def authenticate(self, handler, data=None):
+    async def authenticate(self, handler, data=None):
         if data and data.get("gcube-user"):
             return {"name": data["gcube-user"], "auth_state": data}
         return None
 
-    @gen.coroutine
-    def pre_spawn_start(self, user, spawner):
+    async def pre_spawn_start(self, user, spawner):
         """Pass gcube-token to spawner via environment variable"""
-        auth_state = yield user.get_auth_state()
+        auth_state = await user.get_auth_state()
         if not auth_state:
             # auth_state not enabled
             return
         spawner.environment["GCUBE_TOKEN"] = auth_state["gcube-token"]
         spawner.environment["DATAMINER_URL"] = auth_state["wps-endpoint"]
-        spawner.environment["GCUBE_VRE"] = auth_state["context"]
+        spawner.environment["GCUBE_CONTEXT"] = auth_state["context"]
 
     def get_handlers(self, app):
         return [(r"/login", self.login_handler)]
+
+
+class D4ScienceContextHandler(OAuthLoginHandler):
+    def get_state(self):
+        context = self.get_argument("context", None)
+        self.authenticator.d4science_context = context
+        return super().get_state()
+
+
+class D4ScienceOauthenticator(GenericOAuthenticator):
+    login_handler = D4ScienceContextHandler
+    d4science_oidc_url = Unicode(
+        D4SCIENCE_OIDC_URL,
+        config=True,
+        help="""The OIDC URL for D4science""",
+    )
+    _pubkeys = None
+
+    async def get_iam_public_keys(self):
+        if self._pubkeys:
+            return self._pubkeys
+        discovery_url = url_path_join(
+            self.d4science_oidc_url, ".well-known/openid-configuration"
+        )
+        self.log.debug("Getting OIDC discovery info at %s", discovery_url)
+        http_client = AsyncHTTPClient()
+        req = HTTPRequest(discovery_url, method="GET")
+        try:
+            resp = await http_client.fetch(req)
+        except HTTPError as e:
+            # whatever, get out
+            self.log.warning("Discovery endpoint not working? %s", e)
+            raise web.HTTPError(403)
+        jwks_uri = json.loads(resp.body.decode("utf8", "replace"))["jwks_uri"]
+        self.log.debug("Getting JWKS info at %s", jwks_uri)
+        req = HTTPRequest(jwks_uri, method="GET")
+        try:
+            resp = await http_client.fetch(req)
+        except HTTPError as e:
+            # whatever, get out
+            self.log.warning("Unable to get jwks info: %s", e)
+            raise web.HTTPError(403)
+        self._pubkeys = {}
+        jwks_keys = json.loads(resp.body.decode("utf8", "replace"))["keys"]
+        for jwk in jwks_keys:
+            kid = jwk["kid"]
+            self._pubkeys[kid] = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(jwk))
+        return self._pubkeys
+
+    async def get_uma_token(self, context, audience, access_token, extra_params={}):
+        body = {
+            "grant_type": "urn:ietf:params:oauth:grant-type:uma-ticket",
+            "claim_token_format": "urn:ietf:params:oauth:token-type:jwt",
+            "audience": audience,
+        }
+        body.update(extra_params)
+        http_client = AsyncHTTPClient()
+        req = HTTPRequest(
+            self.token_url,
+            method="POST",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+                "Authorization": f"Bearer {access_token}",
+            },
+            body=urlencode(body),
+        )
+        try:
+            resp = await http_client.fetch(req)
+        except HTTPError as e:
+            # whatever, get out
+            self.log.warning("Unable to get the permission for user: %s", e)
+            raise web.HTTPError(403)
+        self.log.debug("Got UMA ticket from server...")
+        token = json.loads(resp.body.decode("utf8", "replace"))["access_token"]
+        kid = jwt.get_unverified_header(token)["kid"]
+        key = (await self.get_iam_public_keys())[kid]
+        decoded_token = jwt.decode(
+            token,
+            key=key,
+            audience=audience,
+            algorithms=["RS256"],
+        )
+        return token, decoded_token
+
+    async def authenticate(self, handler, data=None):
+        # first get authorized upstream
+        user_data = await super().authenticate(handler, data)
+        context = getattr(self, "d4science_context", None)
+        self.log.debug("Context is %s", context)
+        if not context:
+            self.log.error("Unable to get the user context")
+            raise web.HTTPError(403)
+        access_token = user_data["auth_state"]["access_token"]
+        extra_params = {
+            "claim_token": base64.b64encode(
+                json.dumps({"context": [f"{context}"]}).encode("utf-8")
+            )
+        }
+        token, decoded_token = await self.get_uma_token(
+            context, self.client_id, access_token, extra_params
+        )
+        ws_token, _ = await self.get_uma_token(context, context, access_token)
+        # TODO: add extra checks?
+        permissions = decoded_token["authorization"]["permissions"]
+        user_data["auth_state"].update(
+            {
+                "context_token": ws_token,
+                "permissions": permissions,
+                "context": context,
+            }
+        )
+        return user_data
+
+    async def pre_spawn_start(self, user, spawner):
+        """Pass gcube-token to spawner via environment variable"""
+        auth_state = await user.get_auth_state()
+        if not auth_state:
+            # auth_state not enabled
+            return
+        spawner.environment["GCUBE_TOKEN"] = auth_state["context_token"]
+        # spawner.environment["DATAMINER_URL"] = auth_state["wps-endpoint"]
+        spawner.environment["GCUBE_CONTEXT"] = unquote(auth_state["context"])
+
+
+class D4ScienceSpawner(KubeSpawner):
+    frame_ancestors = Unicode(
+        "https://*.d4science.org 'self'",
+        config=True,
+        help="""Frame ancestors for embedding the hub in d4science""",
+    )
+    sidecar_image = Unicode(
+        "eginotebooks/d4science-storage",
+        config=True,
+        help="""the D4science storage image to use""",
+    )
+
+    def get_args(self):
+        args = super().get_args()
+        tornado_settings = {
+            "headers": {
+                "Content-Security-Policy": "frame-ancestors %s" % self.frame_ancestors
+            },
+            "cookie_options": {"samesite": "None", "secure": True},
+        }
+        # TODO: check if this keeps making sense
+        return [
+            "--SingleUserNotebookApp.tornado_settings=%s" % tornado_settings,
+            "--FileCheckpoints.checkpoint_dir='/home/jovyan/.notebookCheckpoints'",
+            "--FileContentsManager.use_atomic_writing=False",
+            "--NotebookApp.ResourceUseDisplay.track_cpu_percent=True",
+        ] + args
+
+    def auth_state_hook(self, spawner, auth_state):
+        permissions = auth_state["permissions"]
+        # this will filter according to permissions
+        # Assuming that there are no permissions coming from D4Science,
+        # the everything is allowed
+        if spawner.profile_list and permissions:
+            allowed_profiles = [claim["rsname"] for claim in permissions]
+            self.log.debug("allowed profiles: %s", allowed_profiles)
+            spawner.profile_list = list(
+                filter(
+                    lambda x: x.get("profile_type", None) in allowed_profiles,
+                    self.profile_list,
+                )
+            )
+
+    async def pre_spawn_hook(self, spawner):
+        gcube_token = spawner.environment.get("GCUBE_TOKEN", "")
+        context = spawner.environment.get("GCUBE_CONTEXT", "")
+        if context:
+            # set the whole context as annotation (needed for accounting)
+            spawner.extra_annotations["d4science_context"] = context
+            # set only the VRE name in the environment (needed for NFS subpath)
+            vre = context[context.rindex("/") + 1 :]
+            spawner.log.info("VRE: %s", vre)
+            spawner.environment["VRE"] = vre
+        if gcube_token:
+            spawner.extra_containers = [
+                {
+                    "name": "workspace-sidecar",
+                    "image": self.sidecar_image,
+                    "securityContext": {
+                        "privileged": True,
+                        "capabilities": {"add": ["SYS_ADMIN"]},
+                        "runAsUser": 1000,
+                    },
+                    "env": [
+                        {"name": "MNTPATH", "value": "/workspace"},
+                        {"name": "GCUBE_TOKEN", "value": gcube_token},
+                    ],
+                    "volumeMounts": [
+                        {"mountPath": "/workspace:shared", "name": "workspace"},
+                    ],
+                    "lifecycle": {
+                        "preStop": {
+                            "exec": {"command": ["fusermount", "-uz", "/workspace"]}
+                        },
+                    },
+                }
+            ]
