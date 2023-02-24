d64f93a8e916401534e2b66eab8ec23dac4a50a4
Enol Fernandez <enol.fernandez@egi.eu>
2023-02-24 12:31:35 +0000

    Remove D4science gcube token based authentication
    This is no longer in use since long

--- egi_notebooks_hub/d4science.py
+++ egi_notebooks_hub/d4science.py
@@ -21,22 +21,9 @@ from tornado.httpclient import AsyncHTTPClient, HTTPError, HTTPRequest
 from tornado.httputil import url_concat
 from traitlets import Dict, List, Unicode
 
-D4SCIENCE_SOCIAL_URL = os.environ.get(
-    "D4SCIENCE_SOCIAL_URL",
-    "https://api.d4science.org/social-networking-library-ws/rest/",
-)
-D4SCIENCE_PROFILE = "2/people/profile"
 D4SCIENCE_REGISTRY_BASE_URL = os.environ.get(
     "D4SCIENCE_REGISTRY_BASE_URL",
     "https://registry.d4science.org/icproxy/gcube/service",
-)
-D4SCIENCE_DM_REGISTRY_URL = os.environ.get(
-    "D4SCIENCE_REGISTRY_URL",
-    D4SCIENCE_REGISTRY_BASE_URL + "/ServiceEndpoint/DataAnalysis/DataMiner",
-)
-D4SCIENCE_DISCOVER_WPS = os.environ.get(
-    "D4SCIENCE_DISCOVER_WPS",
-    "false",
 )
 D4SCIENCE_OIDC_URL = os.environ.get(
     "D4SCIENCE_OIDC_URL", "https://accounts.d4science.org/auth/realms/d4science/"
@@ -45,129 +32,6 @@ JUPYTERHUB_INFOSYS_URL = os.environ.get(
     "JUPYTERHUB_INFOSYS_URL",
     D4SCIENCE_REGISTRY_BASE_URL + "/GenericResource/JupyterHub",
 )
-
-
-class D4ScienceLoginHandler(BaseHandler):
-    # override implementation of clear_cookies from tornado to add extra
-    # options
-    def clear_cookie(self, name, path="/", domain=None):
-        kwargs = self.settings.get("cookie_options", {})
-        expires = datetime.datetime.utcnow() - datetime.timedelta(days=365)
-        self.set_cookie(
-            name, value="", path=path, expires=expires, domain=domain, **kwargs
-        )
-
-    async def get(self):
-        self.log.debug("Authenticating user")
-        user = await self.get_current_user()
-        token = self.get_argument("gcube-token")
-        if user and token:
-            self.log.debug("Clearing login cookie, new user?")
-            # clear login cookies with full set of options
-
-            self.clear_login_cookie()
-            # make sure we don't do a mess here
-            self.redirect(
-                url_concat(
-                    self.authenticator.login_url(self.hub.base_url),
-                    {"gcube-token": token},
-                ),
-                permanent=False,
-            )
-            return
-        if not token:
-            self.log.error("No gcube token. Out!")
-            raise web.HTTPError(403)
-        http_client = AsyncHTTPClient()
-        # discover user info
-        user_url = url_concat(
-            url_path_join(D4SCIENCE_SOCIAL_URL, D4SCIENCE_PROFILE),
-            {"gcube-token": token},
-        )
-        req = HTTPRequest(user_url, method="GET")
-        try:
-            resp = await http_client.fetch(req)
-        except HTTPError as e:
-            # whatever, get out
-            self.log.warning("Something happened with gcube service: %s", e)
-            raise web.HTTPError(403)
-        resp_json = json.loads(resp.body.decode("utf8", "replace"))
-        username = resp_json.get("result", {}).get("username", "")
-        context = resp_json.get("result", {}).get("context", "")
-
-        if not username or not context:
-            self.log.error("Unable to get the user or context from gcube?")
-            raise web.HTTPError(403)
-
-        # discover WPS if enabled
-        wps_endpoint = ""
-        if D4SCIENCE_DISCOVER_WPS.lower() in ["true", "1"]:
-            self.log.debug("Discover wps")
-            discovery_url = url_concat(
-                D4SCIENCE_DM_REGISTRY_URL, {"gcube-token": token}
-            )
-            req = HTTPRequest(discovery_url, method="GET")
-            try:
-                self.log.debug("fetch")
-                resp = await http_client.fetch(req)
-            except HTTPError as e:
-                # whatever, get out
-                self.log.error("Something happened with gcube service: %s", e)
-                raise web.HTTPError(403)
-            root = ElementTree.fromstring(resp.body.decode("utf8", "replace"))
-            self.log.debug("root %s", root)
-            for child in root.findall(
-                "Resource/Profile/AccessPoint/Interface/Endpoint"
-            ):
-                entry_name = child.attrib["EntryName"]
-                self.log.debug("entry_name %s", entry_name)
-                if entry_name != "GetCapabilities":
-                    wps_endpoint = child.text
-                    self.log.debug("WPS endpoint: %s", wps_endpoint)
-                    break
-        self.log.info("D4Science user is %s", username)
-        data = {
-            "gcube-token": token,
-            "gcube-user": username,
-            "wps-endpoint": wps_endpoint,
-            "context": context,
-        }
-        data.update(resp_json["result"])
-        user = await self.login_user(data)
-        if user:
-            self._jupyterhub_user = user
-            # we need to remove gcube-token from the url query to avoid
-            # the spawner not showing the the options form
-            # this code is basically taken from Stack Overflow
-            # https://stackoverflow.com/a/7734686
-            next_url = urlparse(self.get_next_url(user))
-            query = parse_qs(next_url.query, keep_blank_values=True)
-            query.pop("gcube-token", None)
-            next_url = next_url._replace(query=urlencode(query, True))
-            self.redirect(urlunparse(next_url), permanent=False)
-
-
-class D4ScienceAuthenticator(Authenticator):
-    login_handler = D4ScienceLoginHandler
-
-    async def authenticate(self, handler, data=None):
-        if data and data.get("gcube-user"):
-            return {"name": data["gcube-user"], "auth_state": data}
-        return None
-
-    async def pre_spawn_start(self, user, spawner):
-        """Pass gcube-token to spawner via environment variable"""
-        auth_state = await user.get_auth_state()
-        if not auth_state:
-            # auth_state not enabled
-            return
-        spawner.environment["GCUBE_TOKEN"] = auth_state["gcube-token"]
-        spawner.environment["DATAMINER_URL"] = auth_state["wps-endpoint"]
-        spawner.environment["GCUBE_CONTEXT"] = auth_state["context"]
-
-    def get_handlers(self, app):
-        return [(r"/login", self.login_handler)]
-
 
 class D4ScienceContextHandler(OAuthLoginHandler):
     def get_state(self):
