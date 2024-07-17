04fa11efb9e7cf67720d0609b8ad755e2fc05f7e
Enol Fernandez <enol.fernandez@egi.eu>
2024-07-17 10:00:28 +0100

    Add some debugging

--- egi_notebooks_hub/egiauthenticator.py
+++ egi_notebooks_hub/egiauthenticator.py
@@ -48,6 +48,7 @@ class JWTHandler(BaseHandler):
             resp = await http_client.fetch(req)
         except HTTPClientError as e:
             self.log.warning(f"Unable to get refresh token: {e}")
+            self.log.debug(resp.body)
             return None
         token_info = json.loads(resp.body.decode("utf8", "replace"))
         return token_info.get("refresh_token", None)
