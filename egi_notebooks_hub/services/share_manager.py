800f8ed554e2947204ed8911259ad4d5e4342b50
gonimoro <>
2026-06-19 00:19:57 +0200

    fix: Fixing exception type

--- egi_notebooks_hub/services/share_manager.py
+++ egi_notebooks_hub/services/share_manager.py
@@ -139,7 +139,7 @@ async def call_hub_api(
             r = await method_f(url, headers=headers)
         try:
             r.raise_for_status()
-        except HTTPException:
+        except httpx.HTTPError:
             logger.debug(f"Error from upstream server {r.content}")
             raise HTTPException(r.status_code, detail=r.content.decode())
         try:
